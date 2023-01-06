6329cf02c028a66b84cf39bd31447483a708a754
Ricardo Sandoval Pérez <rsandovalp@uoc.edu>
2023-01-06 09:44:56 +0100

    Hago que la ventana gráfica tenga una dimensión fija la que fijamos en el FXML y no se puede modificar arrastrando las esquinas, para evitar que se deforme.

--- Prueba_2FX/src/main/java/com/example/prueba_2fx/HelloApplication.java
+++ Prueba_2FX/src/main/java/com/example/prueba_2fx/HelloApplication.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -13,10 +12,10 @@ public class HelloApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("OnlineStore");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
